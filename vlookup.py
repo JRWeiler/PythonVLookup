639e184b8e5dca3f79766bd356fc86a3404844ae
unknown <jweiler@cn.edu>
2015-05-19 08:52:13 -0400

    Added comments

--- vlookup.py
+++ vlookup.py
@@ -3,6 +3,7 @@ yRecords = []
 tRecords = []
 output = []
 
+#Function to build an array from values in a csv file
 def buildArray(filename, array):
     rowArray = []
     with open(filename, 'r') as csvInput:
@@ -11,9 +12,13 @@ def buildArray(filename, array):
             rowArray = ', '.join(row).split(',')
             array.append(rowArray)
 
+#Call the buildArray function to populate yRecords and tRecords
 buildArray('yesterday.csv', yRecords)
 buildArray('today.csv', tRecords)
 
+#Create output file and for every record in yRecords, see if there is a match in tRecords
+#If found, write the 'ID' (yRecord[0] in this case, but could also use tRecord[0])
+#and the associated values from each file in a single row delimited by a comma
 with open('output.csv', 'wb') as csvOutput:
     csvWriter = csv.writer(csvOutput, delimiter = ' ', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
     for yRecord in yRecords:
